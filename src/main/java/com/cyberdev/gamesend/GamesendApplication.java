7d8724987cc64fe087c188b9ddfbcf41b43eaa8b
Eneas Junior <eneas.silva.jr@gmail.com>
2023-07-11 00:52:31 -0300

    Project created

---
+++ src/main/java/com/cyberdev/gamesend/GamesendApplication.java
@@ -0,0 +1,13 @@
+package com.cyberdev.gamesend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyberdev/gamesend/GamesendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberdev.gamesend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
